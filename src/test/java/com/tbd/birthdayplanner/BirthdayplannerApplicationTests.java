cb308c8ca9efa39288b11899fa905467605596e7
Banvillet, Louis <Louis.Banvillet@ncr.com>
2017-08-16 14:25:11 +0100

    Added sample spring boot app

---
+++ src/test/java/com/tbd/birthdayplanner/BirthdayplannerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tbd.birthdayplanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BirthdayplannerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
